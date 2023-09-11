1db199067bb20e8b13cb19baad7d3ca237bb2a3b
Godfrey Dhlandhlara <godfreydhl@gmail.com>
2023-09-11 20:32:42 +0200

    schema

---
+++ index.js
@@ -0,0 +1,33 @@
+const { default: mongoose } = require('mongoose')
+
+const User = require('./User')
+
+dotenv = require('dotenv')
+dotenv.config()
+
+
+mongoose.connect(process.env.ATLAS_URI)
+
+run()
+async function run (){
+
+    try{
+        const user = await User.create({
+            name: "Bill",
+            age: 34, 
+        hobbbies: ["Weight Lifting", "Bowling"],
+        address: {
+            street: "Main St"
+        }
+        }) 
+       
+        console.log(user)
+
+    } 
+    catch(e){
+        console.log(e.message)
+    } 
+
+}
+
+ 
